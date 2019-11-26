c07b735a0d15a75651258afc83f7ba3b182b4e38
pyl <a522723936@gmail.com>
2019-11-26 20:52:08 +1100

    make searchBox interact with user input; update css
    Make App as a smart component(container)
    Call jsonplaceholder api to fetch robots
    Change font and background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import './App.css';
-import {robots} from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 
-const App = () =>  {
-    return (
-      <Fragment>
-        <div className='tc'>
-          <h1>RoboFriends</h1>
-          <SearchBox/>
-          <CardList robots={robots}/>
-        </div>
-      </Fragment>
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      robots: [],
+      searchfield:''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+      return response.json();
+    })
+    .then( users =>
+      this.setState({robots:users})
     )
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield:event.target.value})
+  }
+
+  render(){
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    if (this.state.robots.length === 0) {
+      return <h1 className='tc'>Loading</h1>
+    }else{
+      return (
+        <Fragment>
+          <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange = {this.onSearchChange} />
+            <CardList robots={filteredRobots}/>
+          </div>
+        </Fragment>
+      )
+    }
+  }
 }
 
 export default App;
