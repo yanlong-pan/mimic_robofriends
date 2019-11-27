45d276b5ad911889994411930f1b766b646024bc
pyl <a522723936@gmail.com>
2019-11-27 18:33:47 +1100

    Updates

---
+++ precache-manifest.1f9b0e49f87d27c3ba7a0c9593206924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455a150ea325c3760be3982828cc707f",
+    "url": "/mimic_robofriends/index.html"
+  },
+  {
+    "revision": "09d424b756e09aa73c5a",
+    "url": "/mimic_robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d34553fcdd95e1f10606",
+    "url": "/mimic_robofriends/static/css/main.095e2910.chunk.css"
+  },
+  {
+    "revision": "09d424b756e09aa73c5a",
+    "url": "/mimic_robofriends/static/js/2.a2e8f4e5.chunk.js"
+  },
+  {
+    "revision": "d34553fcdd95e1f10606",
+    "url": "/mimic_robofriends/static/js/main.ad2c174e.chunk.js"
+  },
+  {
+    "revision": "634b5b7b7a4031d18f49",
+    "url": "/mimic_robofriends/static/js/runtime-main.ce32fa2a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/mimic_robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
